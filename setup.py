07386980cf8d43675d044ba1a3c597dd21957ed0
Devkrishna Sharma <dev645403@gmail.com>
2026-08-09 01:24:51 +0530

    finally setup complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Devkrishna sharma",
+    author_email="dev645403@gmail.com",
+    packages=find_packages()
+)
